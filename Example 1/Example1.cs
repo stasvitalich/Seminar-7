b0342112055ee7c4c6661c3edf84f73b63500a1b
Stanislav <stasvitalich1994@gmail.com>
2022-09-30 15:12:50 +0600

    Fill in a two-dimensional array by random numbers

--- Example 1/Example1.cs
+++ Example 1/Example1.cs
@@ -1,1 +1,62 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+//Размерность массива: количество строк.
+int n = 0;
+
+//Размерность массива: количество столбцов. 
+int m = 0;
+
+Console.Write("Введите количество столбцов (M): ");
+// Пишем структуру, которая защитит нашу программу от поломки
+// в случае ввода пользователем строки, а не числа
+bool parseMIsOk = int.TryParse(Console.ReadLine(), out int numberM);
+if (!parseMIsOk)
+{
+    Console.WriteLine("Введено значение некорректного формата");
+    return;
+}
+else
+{
+    m = numberM;
+}
+
+Console.Write("Введите количество строк (N): ");
+// Пишем структуру, которая защитит нашу программу от поломки
+// в случае ввода пользователем строки, а не числа
+bool parseNIsOk = int.TryParse(Console.ReadLine(), out int numberN);
+if (!parseNIsOk)
+{
+    Console.WriteLine("Введено значение некорректного формата");
+    return;
+}
+else
+{
+    n = numberN;
+}
+
+// Задаём двумерный массив.
+int[,] array = new int[n, m];
+
+// Заполняем его рандомными числами.
+for (int i = 0; i < n; i = i + 1)
+{
+    for (int j = 0; j < m; j = j + 1)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+
+// Выводим массив на печать.
+for (int i = 0; i < n; i = i + 1)
+{
+    for (int j = 0; j < m; j = j + 1)
+    {
+        Console.Write(array[i, j]);
+    }
+    Console.WriteLine("");
+}
